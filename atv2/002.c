8008dc8cd3069627a06ec0c5dbef69b4cb5f68b9
luizalima13 <142099683+luizalima13@users.noreply.github.com>
2023-08-22 18:15:03 -0300

    Create 002.c

---
+++ atv2/002.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num, dobro;
+
+    printf("\n Digite o numero:");
+    scanf("%d",&num);
+
+    dobro = num * 2;
+
+    printf("\n O dobro é: %d", dobro);
+    return 0;
+}

1d0a3b4efa1b652aac628ac6cbc1634bb480fb32
luizalima13 <142099683+luizalima13@users.noreply.github.com>
2023-10-27 22:18:14 -0300

    Update 001.c

--- av1/001.c
+++ av1/001.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <math.h>
 
+int main (){
+    float x, y;
+
+    printf("valor de x:");
+    scanf("%f", &x);
+
+    y = ((5*x)+3)/sqrt(x*x-16);
+
+    printf("O resultado sera: %.2f", y);
+
+    return 0;
+}

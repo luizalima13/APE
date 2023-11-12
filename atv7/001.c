d2d8e50593dbad09b39c2f22c7ced4a1340c0f7d
luizalima13 <142099683+luizalima13@users.noreply.github.com>
2023-11-12 14:22:10 -0300

    Update 001.c

--- atv7/001.c
+++ atv7/001.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(){
+
+    int n;
+    printf("Digite um numero");
+    scanf("%d", &n);
+
+    for(int i = 0; i <= n; i++ ){
+        printf("\n%d", i);
+    }
+    
+    return 0;
+}

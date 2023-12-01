8cb9296fc007c87a712be0a0e6c54d3fe02a5b03
luizalima13 <142099683+luizalima13@users.noreply.github.com>
2023-12-01 00:06:11 -0300

    Update 001.c

--- atv9/001.c
+++ atv9/001.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int A[6] = {1, 0, 5, -2, -5, 7};
+
+    int soma = A[0] + A[1] + A[5];
+    printf("Soma de A[0], A[1] e A[5]: %d\n", soma);
+
+    A[4] = 100;
+
+    printf("Valores do vetor A:\n");
+    for (int i = 0; i < 6; i++) {
+        printf("%d\n", A[i]);
+    }
+
+    return 0;
+}
 

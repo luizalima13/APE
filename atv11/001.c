a3613d7aee244b5bbf33ad1fbbcaa59a5add7585
luizalima13 <142099683+luizalima13@users.noreply.github.com>
2023-12-07 22:59:30 -0300

    Update 001.c

--- atv11/001.c
+++ atv11/001.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int encontrarMaior(int numero1, int numero2) {
+    return (numero1 > numero2) ? numero1 : numero2;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+
+    int resultado = encontrarMaior(num1, num2);
+
+    printf("O maior numero e: %d\n", resultado);
+
+    return 0;
+}
 
